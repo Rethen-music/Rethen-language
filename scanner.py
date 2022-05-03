a9d9809737f5e93d74fdfbb37af51a9686ebce2c
RETAJD <45774493+RETAJD@users.noreply.github.com>
2022-05-03 15:01:44 +0200

    sound_

--- scanner.py
+++ scanner.py
@@ -3,11 +3,12 @@ f = open("example2.txt", "r")
 tokens = []   # list of all tokens
 ktora_linia = 1
 
-str = ""
-str = str + f.read(1)
-str = str + f.read(1)
+str =""
+#str = str + f.read(1)
+#str = str + f.read(1)
 
-#clef
+#clef"""
+"""
 if (str[1] == 'l'):
     i = 3
     flaga = False
@@ -26,17 +27,120 @@ if (str[1] == 'l'):
 #       (("clef",""))
 #       (("napis","bass"))
 
-
+"""
 #sound   na start mamy 2 znaki
 
 flaga = True
 aktualny_indeks = 1
 
+def loadKey(str):
+    char = f.read(1)
+    temp =""
+    # sharp
+    if char == 's' :
+        temp = 's' + f.read(4)
+        if(temp != "sharp") :
+            while char not in [' ', '', '\n']:
+                str = str + char
+                char = f.read(1)
+            tokens.append(("error", str))
+            return
+        char = f.read(1)
+        if(char != '-' ):
+            while char not in [' ', '', '\n']:
+                str = str + char
+                char = f.read(1)
+            tokens.append(("error", str))
+            return
+    #flat
+    elif char == 'f':
+        temp = 's' + f.read(3)
+        if temp != "flat":
+            while char not in [' ', '', '\n']:
+                str = str + char
+                char = f.read(1)
+            tokens.append(("error", str))
+            return
+        if (char != '-'):
+            while char not in [' ', '', '\n']:
+                str = str + char
+                char = f.read(1)
+            tokens.append(("error", str))
+            return
+    #no sharp no flat
+    if (char != '-'):
+        while char not in [' ', '', '\n']:
+            str = str + char
+            char = f.read(1)
+        tokens.append(("error", str))
+        return
+    str = str + char
+    temp = f.read(5)
+    str = str + temp
+    if( temp in ["minor", "major"] ):
+        i = False
+        while char not in [' ', '', '\n']:
+            i = True
+            str = str + char
+            char = f.read(1)
+        if i:
+            tokens.append(("error", str))
+            str = ""
+        else:
+            tokens.append(("key",str))
+            str = ""
 
+
+char = f.read(1)
+str = str + char
 if str[0] in ['C', 'D', 'E', 'F', 'G', 'A', 'H', 'c', 'd', 'e', 'f', 'g', 'a', 'h']:
-    if str[1] == ' ':
-        tokens.append(("sound", str))
-        flaga = False
+    #char = f.read(1)
+    #if(char == '') : tokens.append(("sound", str))
+
+
+    while char not in [' ', '' , '\n']:
+        char = f.read(1)
+        #str = str + char
+    # TONACJA
+        if(char == '-'):
+            loadKey(str)
+        elif char.isdigit() or char in ['#','b','\\']:
+            i = False
+            str = str + char
+            char = f.read(1)
+            while char not in [' ', '', '\n']:
+                if not char.isdigit(): i = True
+                str = str + char
+                char = f.read(1)
+            if i:
+                tokens.append(("error", str))
+                str = ""
+            else:
+                tokens.append(("sound",str))
+                str = ""
+        else:
+            while char not in [' ', '', '\n']:
+                str = str + char
+                char = f.read(1)
+
+    # C--minor, C-sharp/flat--minor/major
+    # C#
+
+
+
+
+
+    #if str[1] == ' ':
+    #    char = f.read(1)
+     #   if char.isdigit() :
+     #       tokens.append(("sound", str))
+     #       flaga = False
+     #   else:
+     #       tonacja()
+
+
+        # tu moze byc tak samo tonacja    C Major
+
 
     if flaga == True:
         while 1:
