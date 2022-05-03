bef3ee56c6e62bde8c22de18dd64b7224d3781f9
RETAJD <45774493+RETAJD@users.noreply.github.com>
2022-05-03 13:50:32 +0200

    scanner_sound

--- scanner.py
+++ scanner.py
@@ -1,7 +1,7 @@
 f = open("example2.txt", "r")
 
 tokens = []   # list of all tokens
-
+ktora_linia = 1
 
 str = ""
 str = str + f.read(1)
@@ -29,16 +29,39 @@ if (str[1] == 'l'):
 
 #sound   na start mamy 2 znaki
 
-if(str[0] in ['C','D','E','F','G','A','H','c','d','e','f','g','a','h'] ):
-    if(str[1] == ' '): tokens.append(("sound",str))
-    if (str[1] in ['1', '2', '3']):
-        tokens.append(("sound",str))
+flaga = True
+aktualny_indeks = 1
 
-    if(str[1] in ['#','b','\\'] ):
-        str = str + f.read(1)
-        if (str[2] in ['1', '2', '3']):
-            tokens.append(("sound", str))
-    if(str[1] in ['#','b','\\']): tokens.append(("sound",str))
+
+if str[0] in ['C', 'D', 'E', 'F', 'G', 'A', 'H', 'c', 'd', 'e', 'f', 'g', 'a', 'h']:
+    if str[1] == ' ':
+        tokens.append(("sound", str))
+        flaga = False
+
+    if flaga == True:
+        while 1:
+            char = f.read(1)
+            if(char == '' or char == ' '):
+                    break
+            else:
+                str = str + char
+
+        print(str)
+
+
+
+
+
+
+        #if str[1] in ['1', '2', '3']:
+        #    tokens.append(("sound", str))
+        #if str[1] in ['#', 'b', '\\']:
+        #    str = str + f.read(1)
+        #    if (str[2] in ['1', '2', '3']):
+        #        tokens.append(("sound", str))
+        #if str[1] in ['#', 'b', '\\']:
+        #    tokens.append(("sound", str))
+
 
 
 
